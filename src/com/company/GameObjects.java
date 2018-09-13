1227add4a3dd21b3202d8a099d1b9f677a5847ec
egto1016 <egor.tokmakov@netcracker.com>
2018-09-13 17:57:48 +0400

    GameObjects -> locations are added

--- src/com/company/GameObjects.java
+++ src/com/company/GameObjects.java
@@ -1,16 +1,22 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GameObjects {
     private static final List<Combo> combos;
     private static final List<Item> items;
+    private static final List<Location> locations;
 
     // initialize game objects, game objects combination rules
     static {
         combos = new ArrayList<>();
         combos.add(new Combo(null, null, null, ""));
+
+        locations = new ArrayList<>();
+        locations.add(new Location("", "", new Inventory(), new HashMap<Directions, Location>()));
 
         items = new ArrayList<>();
         items.add(new Item("", "", true));
